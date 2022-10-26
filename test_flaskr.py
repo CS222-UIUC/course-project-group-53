63088ffa90bf0219fb3fd3557b107ad64645e996
ruchanandha <90523322+ruchanandha@users.noreply.github.com>
2022-10-25 19:29:49 -0500

    Add files via upload

--- test_flaskr.py
+++ test_flaskr.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+from typing_extensions import assert_never
 
 import pytest
 
@@ -18,3 +19,21 @@ def client():
 
     os.close(db_fd)
     os.unlink(flaskr.app.config['DATABASE'])
+
+def setUp(self):
+        self.app = create_app()
+        self.appctx = self.app.app_context()
+        self.appctx.push()
+
+def tearDown(self):
+        self.appctx.pop()
+        self.app = None
+        self.appctx = None
+
+def test_app(self):
+        assert self.app is not None
+        assert current_app == self.app
+
+
+
+
