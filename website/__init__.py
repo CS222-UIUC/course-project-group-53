9d235ffb30da135f9bea729bb68a1ad618508aa5
jho099 <60435307+jho099@users.noreply.github.com>
2022-10-27 17:52:50 -0500

    updated init py (small)

--- website/__init__.py
+++ website/__init__.py
@@ -36,5 +36,6 @@ def create_app():
 
 def create_database(app):
     if not path.exists('website/' + DB_NAME):
+        #unexpected argument
         db.create_all(app=app)
         print('Created Database!')
